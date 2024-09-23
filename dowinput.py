b5eb7101014a2e0e45ec938d6f8d70e706d282d5
stckibnz345 <zerbymegan@gmail.com>
2024-09-23 22:57:49 +0000

    learning about input function

---
+++ dowinput.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+
+    #userinput
+    user_input=input("What is your username?")
+    #ask dow
+    user_input2=input("What day of the week is it?")
+    #print statement
+    print("Hello," user_input,"!", "Happy", user_input2,"!")
+
+main()
+    
