2ca07c748de4ca3f82d19d2324edd88938dcfc0f
stckibnz345 <zerbymegan@gmail.com>
2024-09-25 15:43:17 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+my_list = [ "192.168.0.5", 5060, "UP" ]
+ip_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print("The first IP in the list: " + ip_list[3] )
+print("The second IP in the list: " + str(ip_list[4]) )
+#print("The last item in the list (state): " + my_list[2] )
+
+
