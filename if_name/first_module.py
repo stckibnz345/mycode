6488fc098f378875f70c56696375b36bed3afd7c
stckibnz345 <zerbymegan@gmail.com>
2024-09-27 17:43:26 +0000

    pip package practice

---
+++ if_name/first_module.py
@@ -0,0 +1,16 @@
+
+print('This code will always execute.')
+
+def main():
+    print('This code belongs to the main function in module 1')
+
+
+if __name__ == '__main__':
+    #print('Code is being run directly from Python.')
+    main()
+
+
+#else:
+    #print('Code is beinf run indirectly from import.')
+
+
---
+++ if_name/second_module.py
@@ -0,0 +1,5 @@
+import first_module
+
+first_module.main()
+
+print("Module #2 Name=", __name__)
