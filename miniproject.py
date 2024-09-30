46fede8b0ce8536a1166522c4427c30ea743d604
stckinnz345 <zerbymegan@gmail.com>
2024-09-30 22:54:54 +0000

    miniproject

---
+++ miniproject.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+    
+    zodiac = input('What is your birth day mm/dd?')
+    month, day = map(int, zodiac.split('/'))
+
+    if (month == 3 and day >= 21
+
+
+
+if __name__= '__main__':
+    main()
